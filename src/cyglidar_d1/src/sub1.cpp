1d76d9c6b7b3ba7669fcf42acab91ff68bff8752
jeahun10717 <jeahun10717@gmail.com>
2023-05-29 18:09:44 +0900

    pub sub code upload

--- src/cyglidar_d1/src/sub1.cpp
+++ src/cyglidar_d1/src/sub1.cpp
@@ -1,4 +1,6 @@
 #include <ros/ros.h>
+#include <string>
+#include <std_msgs/String.h>
 #include <sensor_msgs/PointCloud2.h>
 #include <sensor_msgs/point_cloud2_iterator.h>
 #include <sensor_msgs/LaserScan.h>
@@ -7,27 +9,36 @@
 #include <iostream>
 #include <typeinfo>
 #include <tuple>
+#include <alsa/asoundlib.h>
 
 using namespace std;
 
 int spinCnt = 0;
+int roopCnt;
+
+void publishMessage(ros::Publisher& publisher, const std::string& message) {
+    std_msgs::String msg;
+    msg.data = message;
+
+    // 메시지 발행
+    publisher.publish(msg);
+}
 
 // 메시지 콜백 함수 선언
 //// 처리할 메시지를 포인터로서 파라미터로 전달. 포인터의 자료형은 메시지의 타입을 명시
-void scanCallback(sensor_msgs::PointCloud2ConstPtr const& msg)
+void scanCallback(sensor_msgs::PointCloud2ConstPtr const& msg, ros::Publisher& publisher)
 {
-    
+    roopCnt = 0;
+
     system("clear");
     int a = 0;
     double distance = 0;
     double distSum = 0;
     int count = 0;
     int zeroCnt = 0;
-    int roopCnt = 0;
     double pointArr[60][160];
     double pcArr[60][160];
     tuple<double, double, double> point_tup_arr[60][160];
-
     for (sensor_msgs::PointCloud2ConstIterator<float> it(*msg, "x"); it != it.end(); ++it) {
         cout << fixed;
         cout.precision(2);
@@ -96,26 +107,19 @@ void scanCallback(sensor_msgs::PointCloud2ConstPtr const& msg)
     //     if(i%2 == 0)cout << '\n';
     // }
     double dist = distSum / (double)(100 - zeroCnt);
+    std::string pubMsg = to_string(spinCnt) + '\n';
+
+    publishMessage(publisher, pubMsg);
 }
 
 int main(int argc, char **argv)
 {
-//   ros::init(argc, argv, "listener");		// 노드 이름 초기화
-
-//   ros::NodeHandle nh;
-
-//   // 서브스크라이버 선언
-//   //// 토픽 이름: chatter(publish 노드에서 주는 토픽), 큐 사이즈: 100개
-//   //// 콜백 함수 이름: chatterCallback(위에서 정의함, 여기서 받은 토픽을 처리함)
-//   ros::Subscriber sub = nh.subscribe("/scan_3D", 100, chatterCallback);
-
-//   ros::spin();		// 큐에 요청된 콜백함수를 처리하며, 프로그램 종료시까지 반복함
-
-//   return 0;
     ros::init(argc, argv, "pointcloud2_subscriber");
     ros::NodeHandle nh;
+    
 
-    ros::Subscriber scan_sub = nh.subscribe<sensor_msgs::PointCloud2>("scan_3D", 5, scanCallback);
+    ros::Publisher publisher = nh.advertise<std_msgs::String>("sub_to_pub", 10);
+    ros::Subscriber scan_sub = nh.subscribe<sensor_msgs::PointCloud2>("scan_3D", 5, boost::bind(scanCallback, _1, publisher));
 
     ros::spin();
 
